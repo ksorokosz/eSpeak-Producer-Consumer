79b20cbaa772c0cff633811f88bda65d480216aa
Kamil Sorokosz <mail@mail>
2016-03-13 09:36:30 +0100

    STDIN supported as input to synthesis

--- main.cpp
+++ main.cpp
@@ -1,12 +1,28 @@
 #include "API_eSpeak.h"
+#include <string>
+#include <iostream>
+#include <iterator>
 using namespace std;
+
+class line : public string {};
+
+istream &operator>>(istream &is, line &l)
+{
+    getline(is, l);
+    return is;
+}
 
 int main()
 {
-	string text = "tralalala";
 	API_eSpeak eSpeak = API_eSpeak(API_eSpeak::POLISH);
-
-	eSpeak.synthesis(text.c_str(), text.length() + 1);
-
+	istream_iterator<line> begin(cin);
+	istream_iterator<line> end;
+   
+    for (istream_iterator<line> it = begin; it != end; it++)
+    {
+      	string line = (*it);
+ 		eSpeak.synthesis(line.c_str(), line.length() + 1);
+	}
+    
 	return 0;
 }
